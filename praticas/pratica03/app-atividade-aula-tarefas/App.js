24f8d0969e2e7e663e0b74278e5aac7be312e01f
carolaine730 <carolaine.santos@iesb.edu.br>
2026-08-13 21:31:50 -0300

    subindo atividade pratica/03

---
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View,Button } from 'react-native';
+import {  rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens';
+
+
+export default function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <TextInput placeholder={ rotulo_input_meta}/>
+      <Button title= {rotulo_btn_cadastro_meta}/>
+      <Text>{rotulo_lista_metas}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainContainer:{
+    padding: 30,
+  }
+});
